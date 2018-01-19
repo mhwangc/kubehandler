db137f4514b5641a18e36f2bc71c82195fe3f20e
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2018-01-18 16:50:01 -0800

    Cleaned up replicaTrigger, added kubefunc pkg

---
+++ pkg/kubefunc/replicaUpdate.go
@@ -0,0 +1,53 @@
+package kubefunc
+
+import (
+    "fmt"
+
+    apiv1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+    "k8s.io/client-go/kubernetes"
+    "github.com/hantaowang/kubehandler/pkg/controller"
+    "k8s.io/client-go/util/retry"
+)
+
+// Updates the replica count of Deployment METANAME by the value QUANTITY
+func replicaUpdate(clientset *kubernetes.Clientset, metaname string, quantity string) error {
+    // Getting deployments
+    deploymentsClient := clientset.AppsV1beta1().Deployments(apiv1.NamespaceDefault)
+
+    // Updating deployment
+    retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+        // Retrieve the latest version of Deployment before attempting update
+        // RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
+        result, getErr := deploymentsClient.Get(metaname, metav1.GetOptions{})
+        if getErr != nil {
+            fmt.Errorf("Failed to get latest version of Deployment: %v", getErr)
+            return getErr
+        }
+
+        fmt.Printf("Updating replica count of %v by %v\n", metaname, quantity)
+
+        // Parsing quantity to int32
+        i, err := strconv.ParseInt(quantity, 10, 32)
+        if err != nil {
+            fmt.Errorf("Failed to parse int: %v", quantity)
+            return err
+        }
+
+        // Modify replica count
+        oldRep := result.Spec.Replicas
+        result.Spec.Replicas = int32Ptr(*oldRep + int32(i))
+        if *result.Spec.Replicas < int32(1) {
+            result.Spec.Replicas = int32Ptr(1)
+        }
+        _, updateErr := deploymentsClient.Update(result)
+        return updateErr
+    })
+    if retryErr != nil {
+        fmt.Errorf("Update failed: %v", retryErr)
+        return retryErr
+    }
+    fmt.Printf("Updated replica count of Deployment %v\n", metaname)
+    return nil
+}
--- triggers/replicacosttrigger.go
+++ triggers/replicacosttrigger.go
@@ -1,57 +1,37 @@
 package triggers
 
 import (
-    "fmt"
-
-    apiv1 "k8s.io/api/core/v1"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-    "k8s.io/client-go/kubernetes"
     "github.com/hantaowang/kubehandler/pkg/controller"
-    "k8s.io/client-go/util/retry"
+    "github.com/hantaowang/kubehandler/pkg/kubefunc"
+    "sync/atomic"
+    "fmt"
+    "math"
 )
 
-// ReplicaCount Trigger
-var ReplicasWithinCost = controller.Trigger {
+const costPerPod float64 = 5.0
+const maxCostPerService float64 = 8.0
+
+var ReplicasWithinCostAll = controller.Trigger{
     Name: "ReplicasWithinCost",
-    Desc: "Trigger that ensures that the total cost of a deployment does not surpass the budgeted amount",
-    Satisfied: func(c *controller.Controller, deploymentName string, individualCost int32, totalBudget int32) bool {
-        deploymentsClient := c.Clientset.AppsV1beta1().Deployments(apiv1.NamespaceDefault)
-        retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-            // RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-            deployment, getErr := deploymentsClient.Get(deploymentName, metav1.GetOptions{})
-            return getErr
-        })
-        if retryErr != nil {
-            fmt.Errorf("Failed to get latest version of Deployment: %v", getErr)
-            return nil
+    Desc: fmt.Sprintf("Each service cannot cost more than $%d", maxCostPerService),
+    Satisfied: func(c *controller.Controller) bool {
+        for _, s := range c.Services {
+            if float64(len(s.Pods)) * costPerPod > maxCostPerService {
+                return false
+            }
         }
-        return (*deployment.Spec.Replicas * individualCost) <= totalBudget
+        return true
     },
-    Enforce: func(c *controller.Controller, deploymentName string, individualCost int32, totalBudget int32) bool {
-        deploymentsClient := c.Clientset.AppsV1beta1().Deployments(apiv1.NamespaceDefault)
-        retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-            // RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-            deployment, getErr := deploymentsClient.Get(deploymentName, metav1.GetOptions{})
-            if getErr != nil {
-                fmt.Errorf("Failed to get latest version of Deployment: %v", getErr)
-                return getErr
+    Enforce: func(c *controller.Controller) bool {
+        for _, s := range c.Services {
+            if float64(len(s.Pods)) * costPerPod > maxCostPerService {
+                dif := maxCostPerService - len(s.Pods) * costPerPod
+                delPod := "-" + string(int(math.Ceil(dif / costPerPod)))
+                errRep := kubefunc.replicaUpdate(c.Client, s.Name, delPod)
+                if errRep != nil {
+                    return false
+                }
             }
-            totalReplicas := *deployment.Spec.Replicas
-            totalCost := totalReplicas * individualCost
-            if totalCost <= totalBudget {
-                return true
-            }
-            for totalReplicas * individualCost > totalBudget {
-                totalReplicas--
-            }
-            deployment.Spec.Replicas = &totalReplicas
-            _, updateErr := deploymentsClient.Update(deployment)
-            return updateErr
-        })
-        if retryErr != nil {
-            fmt.Errorf("Failed to enforce ReplicasWithinCost: %v", retryErr)
-            return false
         }
         return true
     },
